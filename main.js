820da06602618f3f16c0086a8da3c736b144c607
TALISHV <127782334+TALISHV@users.noreply.github.com>
2024-07-08 17:52:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+<ol>
+    <div class="test_image_and_result col-xs-12">
+
+    <li class="col-sm-4 col-xs-12">
+        <b> Test Image - </b>
+        <img src="bottle.jpeg">
+    </li>
+
+    <h5 class="col-sm-4 col-xs-12"></h5>
+    Output on<b>Google LENS</b> - 
+    </h5>
+
+    <h5 class="col-sm-4 col-xs-12">
+        Output on <b>Mobilenet Model</b>
+        <span></span>
+            </h5>
+
+            <div class="mini_result" class="col-xs-12">
+                <h4>Result - </h4>
+                <hr>
+                <h4></h4>
+                    </div>   
+</ol>
